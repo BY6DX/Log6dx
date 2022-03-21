62f2fcc690d71cbdd0cb0b00a67e35918368fae7
Qucheng Miao <qcmiao@hotmail.com>
2022-03-21 22:25:44 +0800

    account

--- Server/Models/User.cs
+++ Server/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Server.Models
 {
@@ -8,9 +9,30 @@ namespace Server.Models
         [Required]
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public string Callsign { get; set; }
-        [Required]
+        [Required, EmailAddress]
         public string Email { get; set; }
-        public string Avatar { get; set; } = "/assets/person-fill.svg";
+        public string Callsign { get; set; }
+        public RoleTypes Role { get; set; }
+        public string Avatar { get; set; }
+        [NotMapped]
+        public string DisplayAvatar
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(Avatar))
+                {
+                    return "/assets/person-fill.svg";
+                }
+                return Avatar;
+            }
+        }
+
+        [Flags]
+        public enum RoleTypes
+        {
+            None = 0,
+            Operator = 1 << 0,
+            All = -1
+        }
     }
 }
---
+++ Server/Models/UserActivity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Server.Models
+{
+    public class UserActivity
+    {
+        public Guid Id { get; set; }
+        public User User { get; set; }
+        public string Ip { get; set; }
+        public DateTime Time { get; set; }
+        public ActivityTypes Type { get; set; }
+        public string Remark { get; set; }
+
+        public enum ActivityTypes
+        {
+            Login,
+        }
+    }
+}
